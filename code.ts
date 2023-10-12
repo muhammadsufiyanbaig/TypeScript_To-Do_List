2d8178e4b5eca27350984dc518e6f2c7ebde41c3
Muhammad Sufiyan Baig <139132305+muhammadsufiyanbaig@users.noreply.github.com>
2023-10-12 12:27:02 -0700

    Add files via upload

---
+++ code.ts
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+let todos: string[]= []
+
+async function createTodo(todos: string[]) {
+    do {
+    let ans = await inquirer.prompt({
+        type: "list",
+        message: "Select an operation",
+        name: "select",
+        choices: ["Add", "Update", "Pending", "Remove"]
+    });
+    if (ans.select == "Add") {
+        let AddTodo = await inquirer.prompt({
+            type: "input",
+            message: "Add the New Task",
+            name: "todo",
+        });
+        todos.push(AddTodo.todo);
+        console.log(todos);
+    }
+    if (ans.select == "Update") {
+        let UpdateTodo = await inquirer.prompt({
+            type: "list",
+            message: "Select the task for updating",
+            name: "todos",
+            choices: todos.map(task => task),
+        });
+        let AddTodo = await inquirer.prompt({
+            type: "input",
+            message: "Add the New Task",
+            name: "todo",
+        });
+        let newTodos = todos.filter(val => val !== UpdateTodo.todo);
+        todos = [...newTodos,AddTodo.todo]
+        console.log("Task Updated");
+        console.log(todos);
+    };
+    if (ans.select == "Pending") {
+        console.log(todos);
+        
+    };
+    if (ans.select == "Remove") {
+        let deleteTodo = await inquirer.prompt({
+            type: "list",
+            message: "Select the task for deleting",
+            name: "todos",
+            choices: todos.map(task => task),
+        });
+        let newTodos = todos.filter(val => val !== deleteTodo.todo);
+        todos = [...newTodos]
+        console.log("Task Removed");
+        console.log(todos);
+        
+    };
+} while (true);
+}
+createTodo(todos);
